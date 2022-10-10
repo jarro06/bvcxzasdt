5e46d1e3aa6ad33ee3a9752863df375c37c33234
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-10-10 11:31:02 +0200

    Create maven project

---
+++ src/main/java/de/allianz/bvcxzasdt/BvcxzasdtApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.bvcxzasdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvcxzasdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvcxzasdtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/bvcxzasdt/BvcxzasdtApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.bvcxzasdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvcxzasdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
